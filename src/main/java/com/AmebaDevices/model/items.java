6f2e85ec539b718b620455c27716733bd6656b1e
l.rando <l.rando@contrader.it>
2018-12-12 09:15:01 +0100

    model items trizcustomer labels

---
+++ src/main/java/com/AmebaDevices/model/items.java
@@ -0,0 +1,61 @@
+package com.AmebaDevices.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+
+@Table(name="items",  uniqueConstraints = {@UniqueConstraint(columnNames={"idItems", "commento"})})
+@NoArgsConstructor
+
+public class items {
+
+	@Id
+	@Column
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long idItems;
+	
+
+	
+	@Column (length = 45)
+	@NonNull
+	private String commento;
+	
+	
+	@ManyToOne
+	@JoinColumn (name = " idCustomer")
+    private trizcustomer idCustomer; 
+	
+	@ManyToMany(cascade=CascadeType.ALL)
+	@JoinTable(
+			name= "Cartella",
+			joinColumns= { @JoinColumn(name="idItems")},
+			inverseJoinColumns= {@JoinColumn(name="idLabels")}
+			)
+	Set<labels> projects= new HashSet<>();
+	
+	}
