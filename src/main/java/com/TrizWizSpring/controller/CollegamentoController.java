372abc54ee56e3185674cafb3e9702c485312cfd
l.rando <l.rando@contrader.it>
2019-01-10 11:19:02 +0100

    Visualizza Collegamento

--- src/main/java/com/TrizWizSpring/controller/CollegamentoController.java
+++ src/main/java/com/TrizWizSpring/controller/CollegamentoController.java
@@ -1,23 +1,48 @@
 package com.TrizWizSpring.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.TrizWizSpring.dto.CollegamentoDTO;
+import com.TrizWizSpring.dto.FaseDTO;
+import com.TrizWizSpring.dto.MacroDTO;
+import com.TrizWizSpring.dto.utentiLocaliDTO;
 import com.TrizWizSpring.services.CollegamentoService;
 import com.TrizWizSpring.services.FaseService;
 import com.TrizWizSpring.services.MacroService;
+import com.TrizWizSpring.services.utentiLocaliService;
 
-public class CollegamentoController {
-	@Autowired
-	private FaseService faseService;
-	private MacroService macroService;
-	private CollegamentoService collegamentoService;
-	
-	
-	@Autowired
-	public CollegamentoController(FaseService faseService, MacroService macroService, CollegamentoService collegamentoService ) {
-	   this.faseService=faseService;
-	   this.macroService=macroService;
-	   this.collegamentoService=collegamentoService;
+	@RestController
+	@CrossOrigin
+	@RequestMapping("/Collegamento")
+	public class CollegamentoController {
+		@Autowired
+		private CollegamentoService collegamentoService;
+		
+		
+		@Autowired
+		public CollegamentoController(CollegamentoService collegamentoService ) {
+		   this.collegamentoService=collegamentoService;
+		   
+		}
+	 	
+
+		@CrossOrigin
+		@RequestMapping(value = "/read", method = RequestMethod.GET)
+		public List<CollegamentoDTO> readAll(@RequestParam(value="idFase")long idFase){
+			System.out.println("back endddddd");
+			
+			List<CollegamentoDTO> collegamento = collegamentoService.readCollegamento(idFase);
+			return collegamento;
+		}
+		
+		
+		
 	}
-	
-}
--- src/main/java/com/TrizWizSpring/services/CollegamentoService.java
+++ src/main/java/com/TrizWizSpring/services/CollegamentoService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.TrizWizSpring.converter.CollegamentoConverter;
+import com.TrizWizSpring.converter.FaseConverter;
+import com.TrizWizSpring.converter.MacroConverter;
 import com.TrizWizSpring.dao.CollegamentoDAO;
 import com.TrizWizSpring.dao.FaseDAO;
 import com.TrizWizSpring.dao.ToolDAO;
@@ -13,118 +15,46 @@ import com.TrizWizSpring.dto.FaseDTO;
 import com.TrizWizSpring.dto.ToolDTO;
 import com.TrizWizSpring.model.Collegamento;
 import com.TrizWizSpring.model.Fase;
+import com.TrizWizSpring.model.Macro;
 import com.TrizWizSpring.model.Tool;
 
 
-
+@Service
 public class CollegamentoService {
-/*	
-	private static final String FaseService = null;
-	private static final String ToolService = null;
-	@Autowired
-	private FaseService faseService;
-	private ToolService toolService;
-	//private CustomerDAO customerDAO;
-	@Autowired
-	private ToolDAO toolDAO;
-	@Autowired
-	private FaseDAO faseDAO;
-	@Autowired
-	private CollegamentoDAO collegamentoDAO;
-	
 	
 	@Autowired
-	public CollegamentoService(ToolDAO toolDAO, FaseDAO faseDAO, CollegamentoDAO collegamentoDAO ) {
-		this.toolDAO = toolDAO;
-		this.faseDAO = faseDAO;
-		this.collegamentoDAO= collegamentoDAO;
+	public FaseDAO faseDAO;
+	
+	@Autowired
+	public CollegamentoDAO collegamentoDAO;
+	
+	@Autowired
+	public CollegamentoService(CollegamentoDAO collegamentoDAO) {
+		this.collegamentoDAO = collegamentoDAO;
 	}
-
-	/* public long login(String username, String password) {
-		Customer c = customerDAO.findByUsernameAndPassword(username, password);
-		if (c != null) {
-			if (c.getUserRole() != null)
-				return c.getUserRole();
-		}
-		return -1;
-	} */
-
-/*	public List<LabelDTO> getAll(long idLabel) {
-
-		List<Label> label = new ArrayList<>();
-		Label label1 = this.label = labelDAO.findByIdLabels(idLabel);
-		label1.forEach(l -> toReturn.add(LabelConverter.convertToDto(l)));
-		return toReturn;
-	}
-	public List<LabelDTO> getAll(long attribute) {
-		return (List<LabelDTO>) LabelConverter.convertToDto(labelDAO.findByIdLabels(attribute));
-	}*/
-	/*
 	
-	public List<CollegamentoDTO> readFase(long idFase) {
+	
+	
+
+	public List<CollegamentoDTO> readCollegamento(long idFase) {
 		List<Collegamento> collegamento = new ArrayList<>();
-		List<CollegamentoDTO> toReturn = new ArrayList<>();
+		List<CollegamentoDTO> collegamentoDTO = new ArrayList<>();
 		Fase faseId = this.faseDAO.findOne(idFase);
 		collegamento= collegamentoDAO.findByFase(faseId);
-		collegamento.forEach(t -> toReturn.add(CollegamentoConverter.convertToDto(t)));
-		System.out.println("----------------------------------->" + collegamento.size());
-		return toReturn;
-	}
-	
-	public List<CollegamentoDTO> readTool(long idTool) {
-		List<Collegamento> collegamento = new ArrayList<>();
-		List<CollegamentoDTO> toReturn = new ArrayList<>();
-		Tool toolId = this.toolDAO.findOne(idTool);
-		collegamento= collegamentoDAO.findByTool(toolId);
-		collegamento.forEach(t -> toReturn.add(CollegamentoConverter.convertToDto(t)));
-		System.out.println("----------------------------------->" + collegamento.size());
-		return toReturn;
-	
-	}
-	
-	
-	public void insertCollegamento(CollegamentoDTO collegamento, long idFase) {
-		FaseDTO i= this.faseService.findByPrimaryKey(idFase);
-		collegamento.setFase(i);
-		System.out.println(collegamento.getIdCollegamento() + collegamento.getCommento());
-		collegamentoDAO.save(CollegamentoConverter.convertToCollegamento(collegamento));
-	}
-	
-	public void insertCollegamento1(CollegamentoDTO collegamento, long idTool) {
 		
-		ToolDTO i= this.toolService.findByPrimaryKey(idTool);
-		collegamento.setTool(i);
-		System.out.println(collegamento.getIdCollegamento() + collegamento.getCommento());
-		collegamentoDAO.save(CollegamentoConverter.convertToCollegamento(collegamento));
-	}
-	
-	public List<CollegamentoDTO> readAll() {
-		List<CollegamentoDTO> collegamento = new ArrayList<>();
-		collegamentoDAO.findAll().forEach(c -> {
-
-			collegamento.add(CollegamentoConverter.convertToDto(c));
-		});
-		return collegamento;
-
+		/*for (Collegamento colleg: collegamento) {
+			collegamentoDTO.add(CollegamentoConverter.convertToDto(colleg));
+			System.out.println("ID TOOL"+colleg.getIdCollegamento());
+		}*/
+		
+		collegamento.forEach(f -> collegamentoDTO.add(CollegamentoConverter.convertToDto(f)));
+		System.out.println("----------------------------------->" + collegamento.size());
+		
+		/*for (CollegamentoDTO colleg: collegamentoDTO) {
+			//collegamentoDTO.add(CollegamentoConverter.convertToDto(colleg));
+			System.out.println("ID TOOLDTO"+colleg.getIdCollegamento());
+		}*/
+		return collegamentoDTO;
 		
 	}
-	   public void update(CollegamentoDTO collegamento) {
-			  collegamentoDAO.save(CollegamentoConverter.convertToCollegamento(collegamento));
-		}
-	
-	public void updateCollegamento(CollegamentoDTO collegamento) {
-		collegamentoDAO.save(CollegamentoConverter.convertToCollegamento(collegamento));
-	}
-
-	public CollegamentoDTO searchCollegamento(Long idCollegamento) {
-		return CollegamentoConverter.convertToDto(collegamentoDAO.findOne(idCollegamento));
-	}
-	
-	public void deleteCollegamento(Long idCollegamento) {
-		Collegamento l = collegamentoDAO.findOne(idCollegamento);
-		collegamentoDAO.delete(l);
-	}
-	
-	
-*/
 }
