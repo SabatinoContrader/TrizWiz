18c213a8847567120573cfa2c796f0330aae8690
unknown <m.mellea@contrader.it>
2019-01-10 16:23:31 +0100

    pulizia codice

--- src/models/Owner.ts
+++
@@ -1,9 +0,0 @@
-export class Owner {
-    constructor(
-        public nome: string,
-        public cognome: string,
-        public email: string,
-        public username: string,
-        public userRole: number
-    ) {}
-}
